50da1c20dfd07c4cf58fa0a59d8263a9ab9110bf
ankur.pawar <ankurpawar19@gmail.com>
2016-05-03 09:05:37 +0530

    Nova fractal code improvements.  - Use of meshgrid for generating 2d points.  - corrected the rows and columns in zval. zval rows and columns    represent output image height and width respectively.

--- Fractal/EscapeTime/Newton/novafractal2.m
+++ Fractal/EscapeTime/Newton/novafractal2.m
@@ -1,51 +1,59 @@
-%nova fractal
-%Note: this script takes about 8 seconds to complete
-%the fractal
-res=300;
-xres = res;
-yres = res;
-const=0;
-x = linspace(-0.8,0.8,xres);
-y = linspace(-1.15,0.94,yres);
-c = zeros(length(y),length(x));
-iter= 60;    %number of iterations
-len_x = length(x);
-len_y = length(y);
+function novafractal2
+% Nova fractal
+% This script may take time to complete the fractal depending on the number
+% of iteratons and size of image.
 
-for n=1:len_x
-    c(n,:) = y(n)+i*x(:);
-end
+% ouput image resolution, WIDTHxHEIGHT
+WIDTH = 700; %number of points in x axis
+HEIGHT = 700; %number of points in y axis
 
-h_msg   = msgbox(' Please Wait ',' ');
-zvalues = zeros(len_x,len_y);
-z   = 0+i*0;
-rough = 0;
-rough1 = 0;
+% Fractal x y range
+Y_MIN = -1.15;
+Y_MAX = 0.94;
+X_MIN = -0.8;
+X_MAX = 0.8;
 
-m  =  1;
-n  =  len_x*len_y;
+% maximum number of iterations
+MAX_ITERATION = 64;
 
-for m=1:n
-    const=c(m);
-    k = 0;
-    rough1 = 0.06;
-    rough  = 0;
-    z=c(m);
-    while ((k<=iter)&&(abs(rough1)> 0.05))
-        z = z - 1*(0.33334*z + 0.33334/(z^2))+const;
-        %simplified expression of  z- (z^3-1)/(3*z^2)+c;
-        
-        rough1 = z - rough;
-        rough  = z;
-        k  = k+1;
+TOLERANCE = 0.005;
+
+% Generate linearly spaced points from X_MIN to X_MAX
+x = linspace(X_MIN, X_MAX, WIDTH);
+% Generate linearly spaced points from Y_MIN to Y_MAX
+y = linspace(Y_MIN, Y_MAX, HEIGHT);
+
+% Create a linear 2d grid
+% X is 2d a array, column value are varying and row values are constant
+% Y is 2d a array, row value are varying and column values are constant
+[X Y] = meshgrid(x, y);
+
+% Allocate space for output
+img = zeros(HEIGHT, WIDTH);
+h = msgbox(' Please Wait ',' ');
+nPoint = HEIGHT * WIDTH;
+tic
+for m = 1:nPoint
+    const = i*X(m) + Y(m);
+    k = 1;
+    z0 = const;
+    zn = 0+i*0;
+    while k < MAX_ITERATION
+        zn = z0 - (z0^3+1)/(3*z0^2) + const;
+        if abs(z0 - zn) < TOLERANCE
+            break;
+        end
+        z0 = zn;
+        k = k+1;
     end
-    zvalues(m) =k;
-    
+    img(m) = k;
 end
-
-close(h_msg);
-ho=flipud(colormap(pink));
-colormap((ho));
-image(x,y,zvalues)
-axis tight square
-%imwrite(zvalues,ho,'nova2.png','png');
+toc
+close(h);
+imgMin = min(img(:));
+imgMax = max(img(:));
+cmap = 1 - bone(imgMax);
+colormap(cmap);
+imagesc(img);
+imwrite(img, cmap, 'novafractal2.png', 'png');
+end
