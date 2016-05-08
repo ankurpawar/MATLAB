cec2fd6a2b8ca30013c76883e817f52ad75433aa
ankur.pawar <ankurpawar19@gmail.com>
2016-05-08 08:29:32 +0530

    Correction in animation.  - User input through command line.  - Correct number of rotation.

--- Animation/anim_hypotrochoid.m
+++ Animation/anim_hypotrochoid.m
@@ -1,58 +1,102 @@
 function anim_hypotrochoid
+% Hypotrochoid animation
+% There are no input and output arguments for this function.
+% Run this function and a prompt will appear asking values of
+% r1 = radius of inner circle
+% r2 = radius of rolling circle
+% len = length of line connected to centre of rolling circle
+% Always keep r1 > r2
+% When r2 = len then it becomes hypocycloid
+% Some good choices are r1=10, r2=4, len=4 
+% r1=17, r2=7, len=3
+% r1=47, r2=29, len=13
+MIN_R1 = 10;
+MAX_R1 = 100;
+MIN_R2 = 1;
+MAX_R2 = 100;
+MIN_LEN = 1;
+MAX_LEN = 100;
 
-%animation of HYPOTROCHOID
-%hypotrochoid(r1,r2,len);
-%r1=radius of fixed circle on which smaller circle rolls
-%r2=radius of rolling circle
-%len=line connected to centre of rolling circle
-%example: hypotrochoid(1,0.25,0.5);
-%         hypotrochoid(1,0.25,1);
-%this code also trace hypocycloid
-%when r2=len then it becomes hypocycloid
-%example for hypocycloid: hypotrochoid(1,0.25,0.25);
-%always keep r1>r2
-n=0;
-dtheta=pi/20;  %increase in angle per iteration
-r1=1;          %radius of fixed circle on which smaller circle rolls
-r2=0.1;        %radius of rolling circle
-len=0.1;       %line connected to centre of rolling circle
-roughx=[];     %variable to trace trochoid
-roughy=[];     %variable to trace trochoid
-upto=2*r1*pi/r2+dtheta; %rotate upto
-t=linspace(0,2*pi,75);
+%radius of fixed circle, in which smaller circle rolls
+r1 = getUserInput(['Enter r1 from ' ...
+    num2str(MIN_R1) ' to ' num2str(MAX_R1) '\n'] , MIN_R1, MAX_R1);
 
-[x,y]=pol2cart(t,r2);
-h1=patch(x,y,'r');%draw rolling circle
+%radius of rolling circle
+r2 = getUserInput(['Enter r2 from ' ...
+    num2str(MIN_R2) ' to ' num2str(MAX_R2) '\n'] , MIN_R2, MAX_R2);
 
+%length of line connected to centre of rolling circle
+len = getUserInput(['Enter len from ' ...
+    num2str(MIN_LEN) ' to ' num2str(MAX_LEN) '\n'] , MIN_LEN, MAX_LEN);
 
-[phil,rl]=cart2pol([-len 0],[0 0]);
-h2=line(0,0);%draw line connected to centre of rolling circle
+dtheta = pi/10;  %increase in angle, per iteration
 
-[x_fix,y_fix]=pol2cart(t,r1);
-line(x_fix,y_fix);%draw fixed circle,only once because it is fixed
+draw_hypo(r1,r2,len,dtheta);
+
+end
+
+function draw_hypo(r1,r2,len,dtheta)
+% function to animate rolling circle.
+% nRotations is the number of rotation the rolling circle will do to 
+% complete the hypotrochoid. r1/r2 give the number of rotation only when r2 
+% completely divides r1. lcm(r1,r2)/r2 gives the number rotation for any
+% positive integer where r1 > r2.
+nRotation = lcm(r1,r2)/r2; 
+maxTheta = 2*pi*nRotation + dtheta; % rotate upto this much radian
+t = linspace(0,2*pi,75);
+
+%trochoid trace points
+trocPointx = [];
+trocPointy = [];
+
+[x_roll,y_roll] = pol2cart(t,r2);
+h_roll = patch(x_roll,y_roll,'r'); %draw rolling circle
+
+h_line = line(0,0); %draw line connected to centre of rolling circle
+
+[x_fix,y_fix] = pol2cart(t,r1);
+line(x_fix,y_fix); %draw inner circle only once because it is fixed
 
 axis equal
-axis(r1*[-2 2 -2 2]) %fix axis
+box on
+axis((r1+len)*[-1.2 1.2 -1.2 1.2]) %fix axis
 
-while n<upto && ishandle(h1)
-    [xl,yl]=pol2cart(phil-n,rl); %rotate line
-    [x2,y2]=transform2d(xl,yl,0,-(r1-r2),n*r2/r1,0,r1-r2);
-    set(h2,'xdata',x2,'ydata',y2);%draw line
+h3 = line([0 0],[0 0]);%trace trochoid
+
+n = 0;
+while n < maxTheta && ishandle(h_roll)
+    [xl,yl] = transform2d([-len 0],[0 0],0,0,-n,0,0);
+    [x2,y2] = transform2d(xl,yl,0,-(r1-r2),n*r2/r1,0,r1-r2);
+    set(h_line,'xdata',x2,'ydata',y2);%draw line
     
-    [x,y]=pol2cart(t,r2); %rotate circle
-    [x3,y3]=transform2d(x,y,0,-(r1-r2),n*r2/r1,0,r1-r2);
-    set(h1,'xdata',x3,'ydata',y3);%draw rolling circle
+    [x3,y3] = transform2d(x_roll,y_roll,0,-(r1-r2),n*r2/r1,0,r1-r2);
+    set(h_roll,'xdata',x3,'ydata',y3);%draw rolling circle
     
-    line([roughx x2(1)],[roughy y2(1)]);%trace trochoid
-    roughx=x2(1);
-    roughy=y2(1);
+    trocPointx = [trocPointx x2(1)];
+    trocPointy = [trocPointy y2(1)];
+    set(h3,'xdata',trocPointx,'ydata',trocPointy);
     
-    pause(0.025)
-    n=n+dtheta;
+    pause(0.025);
+    n = n+dtheta;
+end
 end
 
 function [xd,yd]=transform2d(x,y,tx,ty,phi,xr,yr)
-%2d transformation without scaling
-%[xd,yd]=transform2d(x,y,tx,ty,phi,xr,yr)
+% 2d transformation without scaling
+% [xd,yd]=transform2d(x,y,tx,ty,phi,xr,yr)
 xd=x.*cos(phi)-y.*sin(phi)+xr.*(1-cos(phi))+yr.*sin(phi)+tx;
 yd=x.*sin(phi)+y.*cos(phi)+yr.*(1-cos(phi))-xr.*sin(phi)+ty;
+end
+
+function choice = getUserInput(promptStr, minNum, maxNum)
+% return the user input and check the range of input
+choice = input(promptStr);
+if isempty(choice) || ~isnumeric(choice)
+    error('enter a number');
+elseif (choice < minNum) || (choice > maxNum)
+    error(['enter a number between 1 to ' num2str(maxNum)]);
+elseif isfloat(choice)
+    %if choice is floating point value then truncate the fractional part
+    choice = choice - mod(choice,1);
+end
+end
