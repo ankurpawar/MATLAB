fe196318401d0ee93fee239e33095d864c3c2f66
ankur.pawar <ankurpawar19@gmail.com>
2016-05-13 20:07:06 +0530

    Carpet code improvements.  - Binary and indexed image generated using same code.  - Number of color in color map corrected.  - Removed commented code.

--- Fractal/BaseMotif/hafermanCarpet.m
+++ Fractal/BaseMotif/hafermanCarpet.m
@@ -1,12 +1,42 @@
-%Haferman Carpet
-
-a0=uint8([1 1 1;1 1 1;1 1 1]);
-b0=uint8([0 1 0;1 0 1;0 1 0]);
-for n=1:5
-    an=1+[b0 b0 b0;b0 b0 b0;b0 b0 b0];
-    b0=[a0 b0 a0;b0 a0 b0;a0 b0 a0];
-    a0=an;
+function hafermanCarpet(S)
+% Haferman Carpet. Function generates and save haferman carpet image.
+% Input:
+%   hafermanCarpet, binary image is drawn if no input argument are given.
+%   hafermanCarpet(S), where S an integer, if S = 0 then binary(B/W) 
+%   image is drawn else colored image is drawn.
+% Output:
+%   Function has no output. Image drawn is saved in current directory.
+if nargin == 0
+    S = 0;
 end
-imagesc(b0),colormap(bone(4));
-%imwrite(b0,bone(4),'haferman2.png','png'); %save coloured image
-%imwrite(b0,'haferman1.png','bitdepth',1);  %save binary image
+
+nIteration = 5;
+if S == 0
+    x = 0;
+else
+    x = 1;
+end
+
+% generator matrix
+a0 = [1 1 1;1 1 1;1 1 1];
+b0 = [0 1 0;1 0 1;0 1 0];
+
+for n = 1:nIteration
+    an = x + [b0 b0 b0; b0 b0 b0; b0 b0 b0];
+    b0 = [a0 b0 a0; b0 a0 b0; a0 b0 a0];
+    a0 = an;
+end
+
+if S == 0
+    colormap(gray(2));
+    imagesc(b0);
+    imwrite(b0,'hafermanCarpetBW.png','png','bitdepth',1);
+else
+    % min is 0 so number of colors required max + 1
+    maxb0 = max(b0(:));
+    colors = bone(maxb0 + 1);
+    colormap(colors);
+    imagesc(b0);
+    imwrite(uint8(b0),colors,'hafermanCarpetColor.png','png');
+end
+end
--- Fractal/BaseMotif/sierpinskiCarpet.m
+++ Fractal/BaseMotif/sierpinskiCarpet.m
@@ -1,21 +1,38 @@
-%Sierpinski Carpet
-colored=1;
+function sierpinskiCarpet(S)
+% Sierpinski Carpet. Function generates and save sierpinski carpet image.
+% Input:
+%   sierpinskiCarpet, binary image is drawn if no input argument are given.
+%   sierpinskiCarpet(S), where S an integer, if S = 0 then binary(B/W)
+%   image is drawn else colored image is drawn.
+% Output:
+%   Function has no output. Image drawn is saved in current directory.
+if nargin == 0
+    S = 0;
+end
+nIteration = 5;
 
-if colored
-    b0=uint8([1 1 1;1 0 1;1 1 1]);
-    for n=1:5
-        x=zeros(3^n,'uint8');
-        b0=1+[b0 b0 b0;b0 x b0;b0 b0 b0];
+if S == 0
+    b0 = logical([1 1 1;1 0 1;1 1 1]);
+    for n = 1 : nIteration
+        x = logical(zeros(3^n));
+        b0 = [b0 b0 b0;b0 x b0;b0 b0 b0];
     end
-    colors=pink(6);
-    imagesc(b0),colormap(colors);
-    imwrite(b0-1,colors,'sierpinski2.png','png');
+    imagesc(b0,[0 1]);colormap gray
+    imwrite(b0,'sierpinskiBW.png','png','bitdepth',1);
 else
-    b0=logical([1 1 1;1 0 1;1 1 1]);
-    for n=1:5
-        x=logical(zeros(3^n));
-        b0=[b0 b0 b0;b0 x b0;b0 b0 b0];
+    b0 = uint8([1 1 1;1 0 1;1 1 1]);
+    for n = 1 : nIteration
+        x = zeros(3^n,'uint8');
+        a0 = [b0 b0 b0;b0 x b0;b0 b0 b0];
+        b0 = 1+a0;
     end
-    imagesc(b0),colormap(gray(2));
-    %imwrite(bn,'sierpinski1.png','png','bitdepth',1);
+    % Index in PNG image start at 0 but MATLAB index start at 1. Value in
+    % matrix a0 range from 0 to 5, total 6 unique values. So nIteration+1
+    % is passed in pink() to give 6 different colors required to display 
+    % image.
+    colors = pink(nIteration+1);
+    colormap(colors);
+    imagesc(a0);
+    imwrite(a0,colors,'sierpinskiColor.png','png');
+end
 end
